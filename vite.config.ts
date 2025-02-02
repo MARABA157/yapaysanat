393d96a77de0ce7acab12be1f6f9a41d7c0a26cd
MARABA157 <maraba1453@gmail.com>
2025-02-02 12:51:31 +0300

    fix: babel yapılandırması kaldırıldı

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react({
         jsxRuntime: 'automatic',
-        jsxImportSource: 'react',
-        babel: {
-          plugins: [
-            ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]
-          ]
-        }
+        jsxImportSource: 'react'
       }),
     ],
     resolve: {
